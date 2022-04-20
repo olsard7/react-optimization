2466ef3194997b6e54cb5887c3b240305c9abc0f
Oleg <olsard7@gmail.com>
2022-04-20 15:16:19 +0300

    fix mock data

--- src/DATA.js
+++ src/DATA.js
@@ -1,9 +1,6 @@
 // Create mock data with elements containing increasing items
 
-export const data = new Array(100)
-  .fill()
-  .map((_, i) => i + 1)
-  .map((n) => ({
-    id: n,
-    name: `Item ${n}`,
-  }));
+export const data = new Array(100).fill().map((_, i) => ({
+  id: i,
+  name: `Item ${i + 1}`,
+}));
